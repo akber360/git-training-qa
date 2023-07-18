de7d91995eb7765a7395f6bc47e3d6bace6e5048
akber360 <139133081+akber360@users.noreply.github.com>
2023-07-18 14:17:35 +0100

    Create Coonditons.py

---
+++ Coonditons.py
@@ -0,0 +1,18 @@
+devs_money = 100
+dev_can_play_smash = False
+if devs_money > 10 and dev_can_play_smash:
+    print("Dev enters a smash tournament!")
+
+elif devs_money < 10 and dev_can_play_smash:
+    print("Dev is too poor to enter")
+else:
+    print("Dev just can't play smash")
+
+mark = float(input("Enter what marks you recived "))
+
+if mark >85:
+    print("Distinction")
+elif 65<= mark <=85:
+    print("pass")
+else:
+    print ("fail")

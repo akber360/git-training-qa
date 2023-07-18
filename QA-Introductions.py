ef02217b7a3aaf7b3a07217a71f5056c4a352ea3
Akber Ali <akber360@yahoo.co.uk>
2023-07-19 00:21:04 +0100

    Python intro excercise

---
+++ QA-Introductions.py
@@ -0,0 +1,13 @@
+# q1 Create a python file that will input your first name, last name and outputs "Hello [firstname] [lastname]"
+
+#f_name = input("What is your first name? ")
+#l_name = input("What is yout last name? ")
+#print ("Hello",f_name,l_name)
+
+#q2 Open a new window and type the following:
+number1 = float(input("Please enter the first number: "))
+number2 = float(input("Please enter the second number: "))
+answer = number1 + number2
+print(number1, "+", number2, "=", answer)
+# This code takes two inputs from the user which can be decimal places due to using float, it adds
+# up the two inputs and gives them total as answer. 

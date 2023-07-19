2637ee886bc5da7191cafa5bc78595fbe7d71fe9
Akber Ali <akber360@yahoo.co.uk>
2023-07-19 15:02:23 +0100

    debug excerciese 1-4 and tutorial 1-2

---
+++ debuggertut.py
@@ -0,0 +1,16 @@
+import pdb
+
+pdb.set_trace()
+
+a = "aaa"
+b = "bbb"
+c = "ccc"
+
+final = a + b + c
+print(final)
+
+def final(var1,var2,var3):
+    pdb.set_trace()
+    return(var1+var2+var3)
+
+print(final(a,b,c))

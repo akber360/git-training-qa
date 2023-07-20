0e7322d42dd7253e64def87685e2d443f5840115
Akber Ali <akber360@yahoo.co.uk>
2023-07-20 13:28:10 +0100

    asasd

---
+++ python/Shapes-Excercise.py
@@ -0,0 +1,16 @@
+### Polygon
+#Goal: “Create class and sub-class objects which represent different geometrical shapes, such as Rectangles and Squares”
+#Objects should have methods to display area and perimeter
+#like 4
+
+class shapess:
+    def _init_ (type,length,width):
+        pass
+
+
+
+
+
+square = shapess ("square"10,10)
+circle=shapess   ("circle"2,2)
+rectangle=shapess("rectangle"10,5)

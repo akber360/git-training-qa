0d99430f19e861d2aa5319b7989fb84c9689f5e2
Akber Ali <akber360@yahoo.co.uk>
2023-07-21 11:54:30 +0100

    added the Sqaures test and function file

--- python/squares.py
+++ python/squares.py
@@ -5,4 +5,10 @@ def list_of_squares(n):
         a = d
     return d
 akber=list_of_squares(2)
-print (akber)
+akber1=list_of_squares(4)
+akber2=list_of_squares(6)
+akber3=list_of_squares(8)
+akber4=list_of_squares(10)
+akber5=list_of_squares(12)
+
+print (akber,akber1,akber2,akber3,akber4,akber5)

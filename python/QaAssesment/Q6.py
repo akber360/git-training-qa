ad0871cee91cfea204ed7417c02cc455b82daca9
Akber Ali <akber360@yahoo.co.uk>
2023-07-26 22:12:31 +0100

    list

---
+++ python/QaAssesment/Q6.py
@@ -0,0 +1,17 @@
+def six(a1, a2, a3):
+	
+
+    a1 = a1.lower()
+    lennum = len(arg1)
+    a1len = len(a1)
+    (number) = a3 -1 
+#    string_extract = int(a1(number))
+#    listpos = list[a3]
+    return lennum
+
+arg1 = input("Enter word ")
+arg2 = input("Enter letter ")
+arg3 = int(input("Enter number "))
+
+answer = six(arg1,arg2,arg3)
+print(answer)

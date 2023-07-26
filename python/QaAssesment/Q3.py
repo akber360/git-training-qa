e16d7245db81edc198df5811979bb0a98948c873
Akber Ali <akber360@yahoo.co.uk>
2023-07-26 12:36:46 +0100

    string slices

--- python/QaAssesment/Q3.py
+++ python/QaAssesment/Q3.py
@@ -3,12 +3,10 @@ def three(string_low):
     #return string_low
     vowels = "aeiou"
     vowel_count = 0
-
     for char in string_low:
         if char in vowels:
             vowel_count = vowel_count + 1
     return vowel_count
-
 #vowel_count = 0
 #vowels = "aeiou"
 stringss = input("enter text ")

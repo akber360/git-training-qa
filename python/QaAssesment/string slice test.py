46e0c76a067d7e352924c9331af9a1a977e36666
Akber Ali <akber360@yahoo.co.uk>
2023-07-26 12:36:38 +0100

    two ways to slice string

---
+++ python/QaAssesment/string slice test.py
@@ -0,0 +1,17 @@
+
+# Python program to demonstrate
+# string slicing
+ 
+# String slicing
+String = 'ASTRING'
+ 
+# Using slice constructor
+s1 = slice(3)
+s2 = slice(1, 5, 2)
+s3 = slice(-1, -12, -2)
+s4 = String[2]
+print("String slicing")
+print(String[s1])
+print(String[s2])
+print(String[s3])
+print (s4)

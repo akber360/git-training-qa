9c8f6fa09efb62a6bc2f0f7c12fe9f1f8914e343
Akber Ali <akber360@yahoo.co.uk>
2023-07-27 16:33:08 +0100

    adding file functions

---
+++ python/Functions.py
@@ -0,0 +1,19 @@
+#def hello_world(a):
+#    return a +" World"
+#b = "Hello"
+#c = hello_world(b)
+#print(c)
+
+def ra(list01):
+    list01 = [2,3]
+    #print (list01)
+
+def append(list02):
+    list02.append(4)
+    #print (list02)
+
+list03 =[0,1]
+list_test = ra(list03)
+print (list03)
+list_test = append(list03)
+print (list03)

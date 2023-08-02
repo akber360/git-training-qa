ee796995ecc325c581b0365ad6557e5063a6e26d
Akber Ali <akber360@yahoo.co.uk>
2023-08-02 16:45:01 +0100

    javascript excercies

---
+++ Javascript/js_ex1.js
@@ -0,0 +1,15 @@
+let fname = "Akber"
+let lname = "Ali"
+let locate = "London"
+let star = "sun"
+let car = "Mercedes Benz"
+let model = "E63s"
+
+console.log(fname)
+console.log(lname)
+console.log(locate)
+console.log(star)
+
+console.log("My Favourite car is the",car,"and the model is",model)
+console.log("I like the car in %c RED","color:red; font-family:fantasy; font-style: Bold; background-color: black;padding: 10px");
+console.log("This is now %c the end of the exercises.","color: orange;font-family:fantasy; font-style: Bold; background-color: black;padding: 10px");
---
+++ Javascript/test.js
@@ -0,0 +1,18 @@
+// let mytext = "Hello"
+// let mytext2 = "There"
+// console.log(mytext+mytext2);
+//simple console.log() statement
+console.log("Hello World");
+
+let str1 = "Welcome to JavaScript";
+
+console.log(str1);
+
+console.info(str1);
+
+console.warn(str1);
+
+console.error(str1);
+let car = "Audi A5 ";
+let myObj = { str: "Just keep logging you are nearly there ", id:48576 }
+console.info("My first car was a ", car, ". The object is: ", myObj);

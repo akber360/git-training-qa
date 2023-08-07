b443d7a268485ef0532b17b44cb7266714250f9d
Akber Ali <akber360@yahoo.co.uk>
2023-08-07 14:57:11 +0100

    flask

---
+++ flask/numx2.py
@@ -0,0 +1,19 @@
+from flask import Flask
+
+app =Flask(__name__)
+@app.route("/")
+def home():
+    return "something"
+
+@app.route("/number/<int:number>")
+def number(number):
+    number = number *number 
+    return str(number)
+
+@app.route('/about')
+def about():
+    return 'This is the about page'
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="localhost", port=5000)
